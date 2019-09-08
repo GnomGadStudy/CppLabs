3ffd89a11a42d7a1bf0325042fe284ea087e0e68
GnomGad <zhenya.lazorenko@mail.ru>
2019-09-08 20:20:23 +0300

    Test init

---
+++ CppLabs/CppLabs.cpp
@@ -0,0 +1,20 @@
+﻿// 7 вариант
+//<Zhenya><Первая лаба><17:00><08.09.2019>
+
+#include "pch.h"
+#include <iostream>
+
+void processArray();
+
+int main()
+{
+	std::cout << "lle";
+
+	return 0;
+}
+
+void processArray()
+{
+
+}
+
