20a054aeac3382ac12bb2f75de4b61894f8f6649
GnomGad <zhenya.lazorenko@mail.ru>
2019-11-13 09:58:46 +0300

    Add Subtractor

---
+++ Lab3/Subtractor.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include "Subtractor.h"
+
+Subtractor::Subtractor(){
+    setRandomOperands();
+}
+double Subtractor::calculate() {
+    double tmp =0;
+    for(int i;i<length;i++)
+        tmp-= operands[i];
+    
+    return tmp;
+}
+
+void Subtractor::logToScreen(){
+    std::cout<<getString('-')<<std::endl;
+}
+
+void Subtractor::logToFile(const std::string& filename){
+    std::ofstream out;
+    out.open(filename);
+    if (out.is_open())
+    {
+        out << getString('-') << std::endl;
+    }
+    out.close();
+}
---
+++ Lab3/Subtractor.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef SUBTRACTOR_H
+#define SUBTRACTOR_H
+
+#include "ExpressionEvaluator.h"
+
+class Subtractor: public ExpressionEvaluator{
+private:
+    
+public:
+    Subtractor();
+    double calculate() override;
+    void logToScreen() override;
+    void logToFile(const std::string& ) override;
+};
+
+
+
+#endif
